aad09a36c5474e2b99609a6dfa399ffecb1a68f1
손영호 <son-yeongho@son-yeonghoui-MacBookAir.local>
2025-06-15 21:26:25 -0700

    Add user, post, comment models and setup Base

---
+++ app/models/post.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from ..database import Base
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id=Column(Integer, primary_key=True,index=True)
+    title = Column(String, nullable=True)
+    content=Column(Text, nullable=True)
+    owner_id=Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="posts")
+    comments = relationship("Comment", back_populates="post")
---
+++ app/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column,Integer,String
+from sqlalchemy.orm import relationship
+from ..database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=True)
+    hashed_password= Column(String, nullable=True)
+    username=Column(String, unique=True, nullable=True)
+
+    posts=relationship("Post",back_populates="owner")
+    comments=relationship("Comments",back_populates="author")
+    
