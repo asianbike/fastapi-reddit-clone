a7c057471dbcf89a599626f712ed1e7638196ca8
손영호 <lukesson1111@gmail.com>
2025-06-16 21:01:59 -0700

    Add JWT token generation and login schema structure

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from .database import Base, engine
+from .routers import user
 from .models import user, post, comment
 
+user.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
-Base.metadata.create_all(bind=engine)
+app.include_router(user.router)
 @app.get("/")
 def root():
     return {"message": "FastAPI Reddit Clone"}
